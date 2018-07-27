ec100515ab4808c890c3775f1b7bc0c3cc5bda2a
dawson <kjxjx@163.com>
2018-07-27 23:14:03 +0800

    update

--- DaybookServiceImpl.java
+++ DaybookServiceImpl.java
@@ -9,25 +9,26 @@ import javax.annotation.Resource;
 
 import org.springframework.stereotype.Service;
 
-import com.dawson.aaaccount.domain.DayBookMapper;
+import com.dawson.aaaccount.domain.DaybookDao;
 import com.dawson.aaaccount.entity.DayBook;
 
 @Service("daybookService")
 public class DaybookServiceImpl implements DaybookService {
 
 	@Resource
-	private DayBookMapper daybookDao;
+	private DaybookDao daybookDao;
 
 	@Override
 	public List<DayBook> getAll() {
-		return  daybookDao.getAll();
+
+		return daybookDao.getAll();
 	}
 
 	@Override
 	public void addDayBook(DayBook daybook) {
 		DayBook tdb = new DayBook();
-//		tdb.setId(UUID.randomUUID().toString());
-		tdb.setDes("买菜");
+		tdb.setId(UUID.randomUUID().toString());
+		tdb.setDescription("买菜");
 		double money = new Random().nextDouble() * 100;
 //		money = (double)Math.round(money * 100) / 100D;
 		BigDecimal dmoney=new BigDecimal(money);
